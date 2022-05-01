b00b66a480cad9e4d0588eec6a47cf9728b2cc33
Salvini126 <salvini.matteo@iisgalvanimi.edu.it>
2022-05-01 14:56:41 +0000

    nuovo componente per dettagli e variabile nel metodo

--- ver-hotel/src/app/app.module.ts
+++ ver-hotel/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { ListaprenotazioniComponent } from './listaprenotazioni/listaprenotazioni.component';
+import { DettaglistanzaComponent } from './dettaglistanza/dettaglistanza.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaprenotazioniComponent
+    ListaprenotazioniComponent,
+    DettaglistanzaComponent
   ],
   imports: [
     BrowserModule,
